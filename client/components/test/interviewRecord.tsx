23f8ad499f5b8631e3ee59dc995459900d44e16e
Minseok96 <FE.minseok96@gmail.com>
2023-03-10 23:32:07 +0900

    면접영상 녹화기능(초안) 완료

--- client/components/test/interviewRecord.tsx
+++ client/components/test/interviewRecord.tsx
@@ -1,14 +1,22 @@
 import React, { useRef, useEffect, useState } from "react";
-import { Stream } from "stream";
 import styled from "styled-components";
 
 const RecordStyle = styled.div`
-  position: relativ;
+  padding: 30px;
+  position: relative;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  width: 100%;
+  video {
+    border-radius: 15px;
+    border: 8px solid ${({theme}) => theme.colors.primary};
+  }
 `;
+
 const Camera = styled.div`
   position: relative;
-  video {
-  }
+
   button {
     position: absolute;
     bottom: 20px;
@@ -31,64 +39,89 @@ const Camera = styled.div`
 `;
 
 const Result = styled.div`
-  .hasPhoto {
+  position: relative;
+  button {
+    position: absolute;
+    bottom: 20px;
+    left: 20px;
+
+    appearance: none;
+    border: none;
+    outline: none;
+
+    padding: 8px 16px;
+    background-image: linear-gradient(to right, #844fff 50%, #ff4f84 50%);
+    background-position: 0%;
+    background-size: 200%;
+    color: #fff;
+    font-size: 24px;
+    font-weight: 700;
+    transition: 0.4s;
+    cursor: pointer;
   }
 `;
 
 const InterviewRecord = () => {
   const videoRef = useRef<HTMLVideoElement>(null);
-  const photoRef = useRef<HTMLCanvasElement>(null);
-  let mediaRecorder: MediaRecorder | null;
+  const mediaRecorderRef = useRef<MediaRecorder | null>(null);
+  const [recordedChunks, setRecordedChunks] = useState<Blob[]>([]);
 
-  useEffect(() => {
-    getVideo();
-  }, [videoRef]);
-
-  const getUserAccess = navigator.mediaDevices.getUserMedia({
-    video: { width: 1920, height: 1080 },
-    audio: true,
-  });
-
-  const getVideo = () => {
-    getUserAccess
-      .then(stream => {
-        let video = videoRef.current;
-        if (video) {
-          video.srcObject = stream;
-          video.play();
-        }
-      })
-      .catch(err => {
-        console.error(err);
+  const getUserAccess = async () => {
+    try {
+      const stream = await navigator.mediaDevices.getUserMedia({
+        audio: true,
+        video: true,
       });
+      if (videoRef.current) {
+        videoRef.current.srcObject = stream;
+      }
+      return stream;
+    } catch (error) {
+      console.error("유저의 권한을 획득하지 못했습니다.", error);
+      throw error;
+    }
   };
 
   const handleRecord = () => {
-    getUserAccess.then(stream => {
-      mediaRecorder = new MediaRecorder(stream);
-      mediaRecorder.start();
-      console.log(mediaRecorder);
+    getUserAccess().then(stream => {
+      mediaRecorderRef.current = new MediaRecorder(stream, {
+        mimeType: "video/webm",
+      });
+
+      mediaRecorderRef.current.start();
+
+      mediaRecorderRef.current.addEventListener("dataavailable", e => {
+        if (e.data.size > 0) {
+          setRecordedChunks(pre => [e.data,...pre])
+          console.log(recordedChunks);
+        }
+      });
     });
   };
 
   const handleRecordRemove = () => {
-    if (mediaRecorder) {
-      console.log("정지");
-      mediaRecorder.stop();
-      mediaRecorder = null;
+    if (mediaRecorderRef.current) {
+      console.log(mediaRecorderRef.current);
+      mediaRecorderRef.current.stop();
+      mediaRecorderRef.current = null;
     }
   };
 
   return (
     <RecordStyle>
       <Camera>
-        <video ref={videoRef}></video>
-        <button onClick={handleRecord}>Start!</button>
+        <video autoPlay muted ref={videoRef}></video>
+        <button onClick={handleRecord}>Start Recording</button>
       </Camera>
-      <button onClick={handleRecordRemove}>Stop</button>
+      <button onClick={handleRecordRemove}>Stop Recording</button>
       <Result>
-        <canvas ref={photoRef}></canvas>
-        <button>Close</button>
+        {recordedChunks && recordedChunks.length > 0 && (
+          <video
+            autoPlay
+            // controls
+            src={URL.createObjectURL(recordedChunks[0])}
+          ></video>
+        )}
       </Result>
     </RecordStyle>
   );
