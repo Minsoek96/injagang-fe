daa44f0b6f328508e86c7e98d0ab30a6c6a8741b
Minseok96 <FE.minseok96@gmail.com>
2023-04-14 23:47:53 +0900

    인터뷰 질문( 체크 박스 전체 선택, 해제 , 삭제 ) 구현

--- client/components/InterView/InterViewListItem.tsx
+++ client/components/InterView/InterViewListItem.tsx
@@ -1,11 +1,49 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import styled from "styled-components";
+
+const InterViewListItemStyle = styled.div`
+  display: flex;
+  width: 100%;
+  margin-bottom: 3px;
+  input {
+    margin-right: 15px;
+  }
+`;
 
 type InterViewListItemProps = {
   questions: string;
+  allCheck: boolean;
+  id: number;
+  onChange: (id: number, isCheck: boolean) => void;
 };
 
-const InterViewListItem = ({ questions }: InterViewListItemProps) => {
-  return <div>{questions}</div>;
+const InterViewListItem = ({
+  onChange,
+  questions,
+  allCheck,
+  id,
+}: InterViewListItemProps) => {
+  const [isCheck, setIsCheck] = useState<boolean>(false);
+
+  useEffect(() => {
+    if (allCheck) {
+      setIsCheck(true);
+    } else {
+      setIsCheck(false);
+    }
+  }, [allCheck]);
+
+  /**현재 리스트의 체크리스트 변경하고 핸들러를 통해 id값과 체크 상태를 전달 */
+  const handleCheckList = () => {
+    onChange(id, isCheck);
+    setIsCheck(!isCheck);
+  };
+  return (
+    <InterViewListItemStyle>
+      <input type="checkbox" checked={isCheck} onChange={handleCheckList} />
+      <label htmlFor={questions}>{questions}</label>
+    </InterViewListItemStyle>
+  );
 };
 
-export default InterViewListItem;
+export default React.memo(InterViewListItem);
--- client/components/InterView/InterViewListView.tsx
+++ client/components/InterView/InterViewListView.tsx
@@ -12,14 +12,15 @@ import InterViewListItem from "./InterViewListItem";
 import { Card, ScrollBar } from "@/styles/GlobalStyle";
 import AddQustionList from "../Admin/AddTextInput";
 import AddQuestionListView from "../Admin/AddQuestionListView";
+import CustomButton from "../UI/CustomButton";
+import { handleDeleteInterViewQnaList } from "../redux/InterViewQuestion/action";
 
 const InterViewListViewStyle = styled.div``;
-
-const InterViewList_Container = styled.div``;
 
 const Container = styled.div`
   ${ScrollBar}
   width: 100%;
+  height: 90%;
   overflow-x: hidden;
 `;
 
@@ -33,36 +34,104 @@ const InterViewSelectData = [
 
 const InterViewListView = () => {
   const [selectType, setSelectType] = useState<QuestionType | string>("ALL");
+  const [allCheck, setAllCheck] = useState<boolean>(false);
+  const [checkList, setCheckList] = useState<number[]>([]);
   const dispatch = useDispatch();
   const interViewList = useSelector(
     (state: RootReducerType) => state.interViewQuestion.list,
   );
+  const interViewListUpdated = useSelector(
+    (state: RootReducerType) => state.interViewQuestion.isUpdated,
+  );
   const authRole = useSelector((state: RootReducerType) => state.auth.role);
+
+  /**컨트롤 메뉴를 변경시 적용 */
   useEffect(() => {
     dispatch(getInterViewQnaList(selectType));
   }, [selectType]);
 
+  /**인터뷰질문이 업데이트 되면 업데이트 적용 */
+  useEffect(() => {
+    if (interViewListUpdated) {
+      dispatch(getInterViewQnaList(selectType));
+    }
+  }, [interViewListUpdated]);
+
+  /**전체체크 제어 */
+  const handleAllCheck = () => {
+    setAllCheck(!allCheck);
+  };
+
+  /**전체체크 변경된값이 true이면 현재리스트 배열에서 id값만 추출하여 리스트 생성 전체체크 해제시 리스트 초기화*/
+  useEffect(() => {
+    if (allCheck) {
+      const idList = interViewList.map(a => a.id);
+      setCheckList(idList);
+      return;
+    } else {
+      setCheckList([]);
+      return;
+    }
+  }, [allCheck]);
+
+  /**체크여부를 판단후 아이템을 제거하고 추가한다.*/
+  const handleAddCheckList = (id: number, isCheck: boolean) => {
+    if (isCheck) {
+      const removeItem = checkList.filter(a => a !== id);
+      setCheckList(removeItem);
+      return;
+    } else {
+      setCheckList(cur => [...cur, id]);
+      return;
+    }
+  };
+
+  const handleRemoveQuestions = () => {
+    const data = {
+      ids: checkList
+    }
+    setAllCheck(false);
+    dispatch(handleDeleteInterViewQnaList(data));
+  };
+
   return (
     <InterViewListViewStyle>
-      <InterViewList_Container>
+      <Card size={{ height: "450px", width: "300px", flex: "Col" }}>
         <ControlMenu
           value={selectType}
           optionList={InterViewSelectData}
           onChange={setSelectType}
-          Size={{ width: "150px", height: "30px" }}
+          Size={{ width: "100%", height: "30px" }}
         ></ControlMenu>
-        <Card size={{ height: "450px", width: "300px", flex: "Col" }}>
-          <Container>
-            {interViewList &&
-              interViewList.map((a, i) => (
-                <InterViewListItem key={a.id} {...a}></InterViewListItem>
-              ))}
-          </Container>
-        </Card>
+        <Container>
+          {interViewList &&
+            interViewList.map((a, i) => (
+              <InterViewListItem
+                key={a.id}
+                allCheck={allCheck}
+                onChange={handleAddCheckList}
+                {...a}
+              ></InterViewListItem>
+            ))}
+        </Container>
         {authRole === "ADMIN" && (
-          <AddQuestionListView qType={selectType}></AddQuestionListView>
+          <div>
+            <CustomButton
+              onClick={handleAllCheck}
+              text={allCheck ? "전체해제" : "전체선택"}
+              Size={{ width: "100px", font: "15px" }}
+            />
+            <CustomButton
+              onClick={handleRemoveQuestions}
+              text={"삭제하기"}
+              Size={{ width: "100px", font: "15px" }}
+            />
+          </div>
         )}
-      </InterViewList_Container>
+      </Card>
+      {authRole === "ADMIN" && (
+        <AddQuestionListView qType={selectType}></AddQuestionListView>
+      )}
     </InterViewListViewStyle>
   );
 };
