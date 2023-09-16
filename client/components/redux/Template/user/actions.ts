8758cc2dfadcfacc82cb1e811c36ebdd22929e9e
Minsoek96 <fe.minseok96@gmail.com>
2023-09-16 21:44:37 +0900

    fix : ADD_TEMPLAETE -> ADD_TEMPLATE 명칭 실수 수정

--- client/components/redux/Template/user/actions.ts
+++ client/components/redux/Template/user/actions.ts
@@ -1,5 +1,5 @@
 import { IGetTemplate } from "@/types/template/TemplateType";
-import { SET_CUR_TEMPLATE_LIST, ADD_TEMPLAETE_TOGGLE } from "./types";
+import { SET_CUR_TEMPLATE_LIST, ADD_TEMPLATE_TOGGLE } from "./types";
 
 export const setCurTemplateList = (TemplateList: IGetTemplate) => ({
   type: SET_CUR_TEMPLATE_LIST,
@@ -7,6 +7,6 @@ export const setCurTemplateList = (TemplateList: IGetTemplate) => ({
 });
 
 export const addTemplaetToggle = (isAddTemplate:boolean = true) => ({
-  type: ADD_TEMPLAETE_TOGGLE,
+  type: ADD_TEMPLATE_TOGGLE,
   payload: isAddTemplate,
 });
--- client/components/redux/Template/user/types.ts
+++ client/components/redux/Template/user/types.ts
@@ -1,20 +1,18 @@
 import { IGetTemplate } from "@/types/template/TemplateType";
 
 export const SET_CUR_TEMPLATE_LIST = "SET_CUR_TEMPLATE_LIST";
-export const ADD_TEMPLAETE_TOGGLE = "ADD_TEMPLAETE_TOGGLE";
+export const ADD_TEMPLATE_TOGGLE = "ADD_TEMPLAETE_TOGGLE";
 
 export interface IsetCurTemplaetList {
   type: typeof SET_CUR_TEMPLATE_LIST;
   payload: {
-    selectedTemplateList: IGetTemplate
+    selectedTemplateList: IGetTemplate;
   };
 }
 
 export interface IaddTemplateToggle {
-  type: typeof ADD_TEMPLAETE_TOGGLE;
-  payload: {
-    isAddTemplate: boolean;
-  };
+  type: typeof ADD_TEMPLATE_TOGGLE;
+  payload: { isAddTemplate: boolean };
 }
 
 export type userTemplateDispatchType = IsetCurTemplaetList | IaddTemplateToggle;
