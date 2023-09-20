ae9a556fe4e62f9ae60da0d85832b34b9893e34d
Minsoek96 <fe.minseok96@gmail.com>
2023-09-21 02:16:38 +0900

    feat : hasEmptyFielkey 비어있는 필드를 탐색하고 키값을 반환하는 유틸 함수 추가

--- client/util/hasEmpty.ts
+++ client/util/hasEmpty.ts
@@ -2,3 +2,6 @@ export const hasEmptyFields = (obj: { [key: string]: string }): boolean => {
     return Object.values(obj).some(value => value.length === 0);
   };
   
+export   const hasEmptyFieldKey = (obj: { [key: string]: string }) => {
+  return Object.keys(obj).find(key => obj[key] === "");
+};
