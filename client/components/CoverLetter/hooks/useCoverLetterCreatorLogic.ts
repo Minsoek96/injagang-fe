aa7a4f5527920a261b7147b179f8177b304e9e0d
Minsoek96 <fe.minseok96@gmail.com>
2023-10-01 23:39:20 +0900

    feat : 자기소개서 유효검사 추가

--- client/components/CoverLetter/hooks/useCoverLetterCreatorLogic.ts
+++ client/components/CoverLetter/hooks/useCoverLetterCreatorLogic.ts
@@ -8,13 +8,21 @@ const useCoverLetterCreatorLogic = () => {
   const [qnaList, setQnAList] = useState<IReadQnaList[]>([]);
   const dispatch = useDispatch();
 
+  const MIN_QUESTIONS = 1;
+  const MAX_QUESTIONS = 3;
+
+  const coverLetterMinLength = qnaList.length <= MIN_QUESTIONS;
+  const coverLetterMaxLength = qnaList.length >= MAX_QUESTIONS;
+
   const addQnAList = useCallback(() => {
+    if (coverLetterMaxLength) return;
     const newID = uuid4();
     setQnAList(prev => [...prev, { question: "", answer: "", qnaId: newID }]);
-  }, []);
+  }, [qnaList]);
 
   const deleteQnAList = useCallback(
     (targetID: string | number) => {
+      if (coverLetterMinLength) return;
       const filterItem = qnaList.filter(qna => qna.qnaId !== targetID);
       setQnAList(filterItem);
     },
