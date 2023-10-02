82613045570dc9e7bf3817b30701472301665506
Minsoek96 <fe.minseok96@gmail.com>
2023-10-02 23:21:47 +0900

    feat : 유효성 검증 유틸함수 runValidationChecks 추가

---
+++ client/util/runValidationChecks.ts
@@ -0,0 +1,14 @@
+import React from "react";
+
+interface IValid {
+  check: () => boolean;
+  message: string;
+}
+
+export const runValidationChecks = (validation: IValid[]) => {
+  const isValidation = validation.find(item => item.check());
+  if (isValidation) {
+    return isValidation.message;
+  }
+  return false;
+};
