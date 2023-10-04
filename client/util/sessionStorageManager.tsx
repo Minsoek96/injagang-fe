f36a1f1b8fb6bcb744afaf95a87b0ab595d37499
Minsoek96 <fe.minseok96@gmail.com>
2023-10-04 20:32:38 +0900

    feat : sessionStorageManager(세션 스토리지 관련 수행 서비스) 추가

---
+++ client/util/sessionStorageManager.tsx
@@ -0,0 +1,18 @@
+export class SessionStorageManager {
+  private key;
+  constructor(key: string) {
+    this.key = key;
+  }
+
+  get() {
+    const data = sessionStorage.getItem(this.key);
+    return data && JSON.parse(data);
+  }
+  save<T>(data: T) {
+    sessionStorage.setItem(this.key, JSON.stringify(data));
+  }
+
+  delete() {
+    sessionStorage.removeItem(this.key);
+  }
+}
