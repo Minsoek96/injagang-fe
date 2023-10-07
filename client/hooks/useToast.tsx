2654c642e52a050eb328bb0fa9d7ca0184d18cf1
Minsoek96 <fe.minseok96@gmail.com>
2023-10-07 18:03:45 +0900

    refact : useToast 업데이트
    - UI를 TosastItem으로 분리
    - 개별적인 카운팅 애니메이션 추가
    - hideToast 기능 분리

--- client/hooks/useToast.tsx
+++ client/hooks/useToast.tsx
@@ -3,46 +3,44 @@ import styled, { keyframes } from "styled-components";
 import { v4 as uuid4 } from "uuid";
 import { TOAST_MODE } from "@/constants";
 import { fadeIn, fadeOut, slideIn, progressBar } from "@/styles/animations";
+import ToastItem from "./ToastItem";
 
-interface IToast {
+export interface IToast {
   id: string;
   message: string;
+  mode: TOAST_MODE;
+  duration: number;
+  startTime: number;
 }
 
-type TOAST_MODE = (typeof TOAST_MODE)[keyof typeof TOAST_MODE];
-
-const FIXED_DURATION = 3000;
+export type TOAST_MODE = (typeof TOAST_MODE)[keyof typeof TOAST_MODE];
 
 const useToast = (duration: number = 3000) => {
   const [toastList, setToastList] = useState<IToast[]>([]);
-  const [toastMode, setToastMode] = useState<TOAST_MODE>("Info");
 
   const showToast = useCallback(
     (mode: TOAST_MODE = "Info", message: string) => {
-      changeToastMode(mode);
       const id = uuid4();
-      setToastList([...toastList, { id, message }]);
+      const startTime = Date.now();
+      setToastList(pervToastList => [
+        ...pervToastList,
+        { id, message, mode, duration, startTime },
+      ]);
       setTimeout(() => {
-        setToastList(prevList => prevList.filter(toast => toast.id !== id));
+        hideToast(id);
       }, duration);
     },
-    [toastList],
+    [],
   );
 
-  const changeToastMode = useCallback(
-    (mode: TOAST_MODE) => {
-      setToastMode(mode);
-    },
-    [toastMode],
-  );
+  const hideToast = (id: string) => {
+    setToastList(prevList => prevList.filter(toast => toast.id !== id));
+  };
 
   const RenderToast: React.FC = () => (
     <ToastContainer>
       {toastList.map(toast => (
-        <ToastMessage key={toast.id} mode={toastMode}>
-          {toast.message}
-          <ProgressBar duration={FIXED_DURATION} />
-        </ToastMessage>
+        <ToastItem key={toast.id} {...toast} />
       ))}
     </ToastContainer>
   );
@@ -55,40 +53,4 @@ const ToastContainer = styled.div`
   bottom: 20px;
   right: 20px;
   z-index: 1000;
-`;
-
-const ToastMessage = styled.div<{ mode: TOAST_MODE }>`
-  position: relative;
-  background-color: ${props => {
-    switch (props.mode) {
-      case "SUCCESS":
-        return "#BDFCC9";
-      case "WARNING":
-        return "#ffc107";
-      case "ERROR":
-        return "#dc3545";
-      case "INFO":
-      default:
-        return "#17a2b8";
-    }
-  }};
-  color: black;
-  font-size: 1.2rem;
-  padding: 10px 20px;
-  margin-top: 10px;
-  border-radius: 5px;
-  border: 2px solid white;
-  box-shadow: 0 4px 12px rgba(0, 0, 0, 0.1);
-  animation: ${slideIn} 0.5s forwards, ${fadeIn} 0.5s,
-    ${fadeOut} 0.5s ${FIXED_DURATION - 500}ms;
-  will-change: opacity;
-`;
-
-const ProgressBar = styled.div<{ duration: number }>`
-  position: absolute;
-  bottom: 0;
-  left: 0;
-  height: 5px;
-  background-color: #f46a6a;
-  animation: ${progressBar} ${props => props.duration}ms linear;
 `;
