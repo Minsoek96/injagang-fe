17cf31272026c58b4d6b5ccc06b12c3c199129c0
Minsoek96 <fe.minseok96@gmail.com>
2023-10-11 02:05:13 +0900

    feat : 유저가 선택한 예상 질문 관련 상태를 관리하기 위한 커스텀훅 추가
    - 리덕스에 대한 컴포넌트의 의존도를 낮추기 위함
    - 관련 로직의 관리의 편리성을 위함

---
+++ client/components/InterViewQuestion/hooks/useEUserQuestionManager.ts
@@ -0,0 +1,38 @@
+import React, { useCallback, useEffect } from "react";
+import {
+  setSelectedQuestionsAction,
+  setTypeAction,
+} from "@/components/redux/InterViewQuestion/user/action";
+import { RootReducerType } from "@/components/redux/store";
+import { QuestionType } from "@/types/InterViewQuestion/InterViewQuestionType";
+import { useDispatch } from "react-redux";
+import { useSelector } from "react-redux";
+import useExpectedQuestionManager from "./useExpectedQuestionManager";
+
+const useEUserQuestionManager = () => {
+  const dispatch = useDispatch();
+  const { selectedQuestions, selectedType } = useSelector(
+    (state: RootReducerType) => state.userInterViewQuestions,
+  );
+  const { dispatchGetInterViewQnaList } = useExpectedQuestionManager();
+
+  useEffect(() => {
+    dispatchGetInterViewQnaList(selectedType);
+  }, [selectedType]);
+
+  const dispatchSelectedType = useCallback((type: QuestionType | string) => {
+    dispatch(setTypeAction(type));
+  }, []);
+
+  const dispatchSetSelectedQuestions = useCallback((list: string[]) => {
+    dispatch(setSelectedQuestionsAction(list));
+  }, []);
+  return {
+    selectedQuestions,
+    selectedType,
+    dispatchSelectedType,
+    dispatchSetSelectedQuestions,
+  };
+};
+
+export default useEUserQuestionManager;
