d8411a9ddefe4f5a118d498254e1c6f975dfd3f3
Minsoek96 <fe.minseok96@gmail.com>
2023-10-11 20:08:51 +0900

    feat : QuestionSelector 추가

---
+++ client/components/InterViewQuestion/ExpectedQuestion/QuestionSelector.tsx
@@ -0,0 +1,34 @@
+import React, { useState, useEffect } from "react";
+import ControlMenu from "@/components/UI/ControlMenu";
+import { QuestionType } from "@/types/InterViewQuestion/InterViewQuestionType";
+
+const InterViewSelectData = [
+  { title: QuestionType.CS, id: 1 },
+  { title: QuestionType.SITUATION, id: 2 },
+  { title: QuestionType.JOB, id: 3 },
+  { title: QuestionType.PERSONALITY, id: 4 },
+  { title: "ALL", id: 5 },
+];
+
+interface IQuestionSelectorProps {
+  selectedType: QuestionType | string;
+  onChange: (type: QuestionType | string) => void;
+}
+
+const QuestionSelector = ({
+  selectedType,
+  onChange,
+}: IQuestionSelectorProps) => {
+  return (
+    <div>
+      <ControlMenu
+        value={selectedType}
+        optionList={InterViewSelectData}
+        onChange={onChange}
+        Size={{ width: "100%", height: "30px" }}
+      ></ControlMenu>
+    </div>
+  );
+};
+
+export default React.memo(QuestionSelector);
