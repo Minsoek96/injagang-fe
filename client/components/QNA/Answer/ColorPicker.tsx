4cce13c77fb134141ad57419976e9e933b1419f5
Minsoek96 <fe.minseok96@gmail.com>
2023-10-16 23:24:50 +0900

    feat : ColorPicker 색상을 선택하는 메뉴바 추가

---
+++ client/components/QNA/Answer/ColorPicker.tsx
@@ -0,0 +1,36 @@
+import React from "react";
+import styled from "styled-components";
+
+interface ColorPickerProps {
+  onSelectColor: (color: string) => void;
+}
+
+const colors = ["#ff0000b3", "#0c750c9f", "#0000FF", "#e69a0def"];
+
+const ColorPicker = ({ onSelectColor }: ColorPickerProps) => {
+  return (
+    <ColorPickerContainer>
+      {colors.map(color => (
+        <ColorBox
+          key={color}
+          color={color}
+          onClick={() => onSelectColor(color)}
+        />
+      ))}
+    </ColorPickerContainer>
+  );
+};
+
+const ColorPickerContainer = styled.div`
+  display: flex;
+  gap: 10px;
+`;
+
+const ColorBox = styled.div<{ color: string }>`
+  width: 20px;
+  height: 20px;
+  background-color: ${props => props.color};
+  cursor: pointer;
+`;
+
+export default ColorPicker;
