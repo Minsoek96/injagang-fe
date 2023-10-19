d7bd6a0864fec4c826e4ff803c6ae8d758d08a3f
Minsoek96 <fe.minseok96@gmail.com>
2023-10-20 00:51:03 +0900

    feat : PageNation 컴포넌트 업데이트
    - usePageNation을 추가하여 로직을 분리
    - 현재 페이지 정보 전달에만 관여

--- client/components/QNA/PageNation.tsx
+++ client/components/QNA/PageNation.tsx
@@ -1,93 +1,57 @@
 import React, { useEffect, useState } from "react";
-import { useDispatch, useSelector } from "react-redux";
-import { getBoardList } from "../redux/QnA/actions";
-import { InitiaState } from "../redux/QnA/reducer";
-import { RootReducerType } from "../redux/store";
 import CustomButton from "../UI/CustomButton";
+import useQnaManager from "./hooks/useQnaManager";
+import usePageNation from "@/hooks/usePageNation";
 
 const PageNation = () => {
-  const [curPageNumber, setCurPageNumber] = useState(1);
-  const [totalPage, setTotalPage] = useState(0);
-  const [pageMoveNumberLimit, setPageMoveNumberLimit] = useState(8);
-  const [minPageNumLimit, setMinPageNumLimit] = useState(0);
-  const [maxPageNumLimit, setMaxPageNumLimit] = useState(8);
-
-  const { boardInFoList } = useSelector(
-    (state: RootReducerType) => state.board,
-  );
-  const boardIsUpdated = useSelector(
-    (state: RootReducerType) => state.board.isUpdated,
-  );
-  const dispatch = useDispatch();
-  useEffect(() => {
-    setTotalPage(boardInFoList.totalPage);
-  }, [boardInFoList]);
-
-  // 페이지가 변경될때마다 새로운 페이지리스트 호출
-  useEffect(() => {
-    dispatch(getBoardList(curPageNumber));
-  }, [curPageNumber]);
+  const { boardInFoList, isUpdated, dispatchGetBoardList } = useQnaManager();
+  const totalPage = boardInFoList.totalPage;
+  const {
+    curPageNum,
+    handlePageClick,
+    handlePrevClick,
+    handleNextClick,
+    visiblePageNumbers,
+  } = usePageNation([totalPage, 8]);
 
   useEffect(() => {
-    if (boardIsUpdated) {
-      dispatch(getBoardList(curPageNumber));
-    }
-  }, [boardIsUpdated]);
+    dispatchGetBoardList(curPageNum);
+  }, [curPageNum, isUpdated]);
 
-  const handleBtnClick = (idx: number) => {
-    setCurPageNumber(idx + 1);
-  };
-
-  /** 이전페이지 호출와 페이지넘김을 담당*/
-  const handlePreBtn = () => {
-    if (curPageNumber > 1) {
-      setCurPageNumber(curPageNumber - 1);
-      if (curPageNumber === minPageNumLimit + 1) {
-        setMinPageNumLimit(minPageNumLimit - pageMoveNumberLimit);
-        setMaxPageNumLimit(maxPageNumLimit - pageMoveNumberLimit);
-      }
-    }
-  };
-
-  /** 다음페이지 호출와 페이지넘김을 담당 */
-  const handleNextBtn = () => {
-    if (totalPage > curPageNumber) {
-      setCurPageNumber(curPageNumber + 1);
-      if (totalPage >= maxPageNumLimit && curPageNumber === maxPageNumLimit) {
-        setMinPageNumLimit(minPageNumLimit + pageMoveNumberLimit);
-        setMaxPageNumLimit(maxPageNumLimit + pageMoveNumberLimit);
-      }
-    }
-  };
   return (
     <div>
-      <CustomButton
-        Size={{ width: "40px", font: "15px" }}
-        onClick={() => handlePreBtn()}
-        text={"<"}
-      />
-      {totalPage !== 0 &&
-        Array(totalPage)
-          .fill(1)
-          .map((page, idx) => {
-            if (idx + 1 > minPageNumLimit && idx + 1 < maxPageNumLimit + 1)
-              return (
-                <CustomButton
-                  className={idx + 1 === curPageNumber ? "active_button" : ""}
-                  key={page}
-                  Size={{ width: "40px", font: "15px" }}
-                  onClick={() => handleBtnClick(idx)}
-                  text={`${idx + 1}`}
-                />
-              );
-          })}
-      <CustomButton
-        Size={{ width: "40px", font: "15px" }}
-        onClick={() => handleNextBtn()}
-        text={">"}
-      />
+      <NavigationButton text="<" onClick={handlePrevClick} />
+      {visiblePageNumbers.map(pageNum => (
+        <NavigationButton
+          key={pageNum}
+          text={`${pageNum}`}
+          isActive={pageNum === curPageNum}
+          onClick={() => handlePageClick(pageNum)}
+        />
+      ))}
+      <NavigationButton text=">" onClick={handleNextClick} />
     </div>
   );
 };
+
+//TODO :: 스타일 리팩토링 작업시 정리 할 것!!
+interface NavigationButtonProps {
+  text: string;
+  isActive?: boolean;
+  onClick: () => void;
+}
+
+const NavigationButton = ({
+  text,
+  isActive = false,
+  onClick,
+}: NavigationButtonProps) => (
+  <CustomButton
+    Size={{ width: "40px", font: "15px" }}
+    className={isActive ? "active_button" : ""}
+    onClick={onClick}
+    text={text}
+  />
+);
 
 export default React.memo(PageNation);
