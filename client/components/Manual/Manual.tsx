8a507a34d18aa921d2155b27485fa3a964988daf
Minsoek96 <fe.minseok96@gmail.com>
2023-10-23 20:02:03 +0900

    chore : 머지를 위한 커밋정리 ( 추후 계획 )
    - 홈 메인 Manual 렌더링 계획 수립중
    - 첫방문 유저 체크 ? 메뉴얼 안내 : 메인 화면 안내

---
+++ client/components/Manual/Manual.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import ManualCheckList from "./ManualCheckList";
+
+const Manual = () => {
+  return (
+    <div>
+      <h2>인자강 방문은 처음 이신가요?</h2>
+      <ManualCheckList />
+    </div>
+  );
+};
+
+export default Manual;
---
+++ client/components/Manual/ManualCheckList.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import styled from "styled-components";
+
+const ManualCheckList = () => {
+  return <CheckInput type={"checkbox"}></CheckInput>;
+};
+
+export default ManualCheckList;
+
+const CheckInput = styled.input`
+  &[type="checkbox"] {
+  }
+`;
