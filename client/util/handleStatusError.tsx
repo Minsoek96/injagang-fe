c9fc8c7015ea90868e0f07547f0137a67f70b6bb
Minsoek96 <fe.minseok96@gmail.com>
2023-11-08 23:21:13 +0900

    feat : 재시도 API 추가
    - 불변성을 유지하며 header의 상태를 업데이트후 요청

--- client/util/handleStatusError.tsx
+++ client/util/handleStatusError.tsx
@@ -1,17 +1,12 @@
+import { API } from "@/api/client";
 import ErrorMessage from "@/components/ErrorMessage";
-import Router from "next/router";
+import { AxiosRequestConfig } from "axios";
 
-export const handleStatusError = (status: number = 1) => {
+export const handleStatusError = (status: number = 1, reTry: () => void) => {
   let errorMessage = "";
-  let onAction = () => {
-    Router.reload();
-  };
   switch (status) {
     case 401:
       errorMessage = "인증이 만료되었습니다. 다시 로그인해주세요.";
-      onAction = () => {
-        Router.back();
-      }
       break;
     case 500:
       errorMessage = "서버 오류가 발생했습니다. 관리자에게 문의하세요.";
@@ -20,7 +15,22 @@ export const handleStatusError = (status: number = 1) => {
       errorMessage = "서버와 연결이 끊겼습니다. 잠시후 다시 시도해주세요.";
       break;
   }
-  return (
-    <ErrorMessage message={errorMessage} onAction={onAction}></ErrorMessage>
-  );
+  return <ErrorMessage message={errorMessage} onAction={reTry}></ErrorMessage>;
+};
+
+export const reTryAPI = async (
+  originRequest?: AxiosRequestConfig,
+): Promise<void> => {
+  if (!originRequest) {
+    // error.config가 undefined인 경우에 대한 처리를 추가
+    throw new Error("재시도를 위한 설정이 없습니다.");
+  }
+  const updateRequest: AxiosRequestConfig = {
+    ...originRequest,
+    headers: {
+      ...originRequest.headers,
+      Retry: "true",
+    },
+  };
+  return await API.request(updateRequest);
 };
