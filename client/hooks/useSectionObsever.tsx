aca0890ebf01414483a384c3400a9e470c769330
Minsoek96 <fe.minseok96@gmail.com>
2023-12-09 01:58:05 +0900

    feat : useSectionObserver훅 추가
    - 관측 대상을 지정하는 ref를 반환
    - 관측 대상의 발견시 콜백, 가시성을 인자로 받음

---
+++ client/hooks/useSectionObsever.tsx
@@ -0,0 +1,30 @@
+import React, { useCallback, useEffect, useRef, useState } from "react";
+
+/**관측대상 아이템을 부여하고 감시하는 훅 */
+const useSectionObsever = (callback: () => void, options: number) => {
+  const targetItemRef = useRef<HTMLDivElement|null>(null);
+  const memoCallback = useCallback(callback, []);
+  useEffect(() => {
+    const observer = new IntersectionObserver(
+      entries => {
+        if (entries[0].isIntersecting) {
+          memoCallback();
+        }
+      },
+      { threshold: options },
+    );
+
+    const currentRef = targetItemRef.current;
+    if (currentRef) {
+      observer.observe(currentRef);
+    }
+    return () => {
+      if (currentRef) {
+        observer.disconnect();
+      }
+    };
+  }, [callback, options]);
+
+  return { targetItemRef };
+};
+export default useSectionObsever;
