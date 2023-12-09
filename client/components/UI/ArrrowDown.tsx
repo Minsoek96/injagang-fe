d23c5453da259a6d620f7d4244e369b2f345c482
Minsoek96 <fe.minseok96@gmail.com>
2023-12-10 03:13:00 +0900

    feat : 화살표 아래 표시 컴포넌트 추가

---
+++ client/components/UI/ArrrowDown.tsx
@@ -0,0 +1,37 @@
+import React from "react";
+import styled, { keyframes, css } from "styled-components";
+import { BsArrowDown } from "react-icons/bs";
+
+const ArrrowDown = () => {
+  return (
+    <ArrowContainer>
+      <BsArrowDown />
+    </ArrowContainer>
+  );
+};
+
+export default ArrrowDown;
+
+const bounce = keyframes`
+  0%, 100% {
+    transform: translateY(0);
+  }
+  50% {
+    transform: translateY(-15px);
+  }
+`;
+
+const ArrowContainer = styled.div`
+  position: fixed;
+  bottom: 8rem;
+  left: 50%;
+  transform: translateX(-50%);
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  animation: ${bounce} 2s infinite;
+  opacity: 0.8;
+  svg {
+    font-size: 3.5rem;
+  }
+`;
