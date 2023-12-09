dbc5198f356212f6a3a4e583b03997b44af0287d
Minsoek96 <fe.minseok96@gmail.com>
2023-12-10 03:14:39 +0900

    feat : Manual 컴포넌트 초안 작성
    - 홈페이지 메인 페이지 컴포넌트
    - 기능 상세 설명서 제공 예정

--- client/components/Manual/Manual.tsx
+++ client/components/Manual/Manual.tsx
@@ -1,11 +1,42 @@
-import React from "react";
-import ManualCheckList from "./ManualCheckList";
+import React, { Suspense } from "react";
+import goodInterView from "@/public/assets/main.jpg";
+import interViewScreen from "@/public/assets/면접.png";
+import communityScreen from "@/public/assets/커뮤니티.png";
+
+const manualData = [
+  {
+    main: "인터뷰와 자소서를 강하게 어필하자",
+    sub: "당신을 서포트 하겠습니다.",
+    imageList: [goodInterView],
+  },
+  {
+    main: "자소서 피드백에 특화된 커뮤니티",
+    sub: "자소서를 강력하게",
+    imageList: [communityScreen],
+  },
+  {
+    main: "준비된 예상 질문으로 인터뷰 연습",
+    sub: "인터뷰를 강력하게",
+    imageList: [interViewScreen],
+  },
+];
+const ManualItems = React.lazy(() => import("./ManualItems"));
 
 const Manual = () => {
+  const lastSection = manualData.length - 1;
   return (
     <div>
-      <h2>인자강 방문은 처음 이신가요?</h2>
-      <ManualCheckList />
+      <Suspense fallback={<p>로딩</p>}>
+        {manualData.map((item, index) => (
+          <ManualItems
+            key={index}
+            mainTitle={item.main}
+            subTitle={item.sub}
+            isArrow={index >= lastSection ? false : true}
+            imageList={item.imageList}
+          ></ManualItems>
+        ))}
+      </Suspense>
     </div>
   );
 };
---
+++ client/components/Manual/ManualItems.tsx
@@ -0,0 +1,98 @@
+import React, { useState } from "react";
+import Image, { StaticImageData } from "next/image";
+import styled, { keyframes, css } from "styled-components";
+import useSectionObsever from "@/hooks/useSectionObsever";
+import ArrrowDown from "../UI/ArrrowDown";
+
+interface MnualItemsProps {
+  mainTitle: string;
+  subTitle: string;
+  isArrow: boolean;
+  imageList: StaticImageData[];
+}
+
+const ManualItems = ({
+  mainTitle,
+  subTitle,
+  isArrow,
+  imageList,
+}: MnualItemsProps) => {
+  const [isShow, setIsShow] = useState<boolean>(false);
+  const { targetItemRef } = useSectionObsever(() => {
+    !isShow && setIsShow(true);
+  }, 0.5);
+  return (
+    <BannerContainer ref={targetItemRef} isShow={isShow}>
+      <MainTitle isShow={isShow}>{mainTitle}</MainTitle>
+      <SubTitle isShow={isShow}>{subTitle}</SubTitle>
+      {imageList.map((image, index) => (
+        <ImageContainer key={index}>
+          <Image
+            src={image}
+            alt="Interview Image"
+            width={700}
+            height={700}
+            loading="lazy"
+          />
+        </ImageContainer>
+      ))}
+      {isArrow && <ArrrowDown />}
+    </BannerContainer>
+  );
+};
+
+export default ManualItems;
+const fadeInUp = keyframes`
+  from {
+    opacity: 0;
+    transform: translateY(20px);
+  }
+  to {
+    opacity: 1;
+    transform: translateY(0);
+  }
+`;
+const fadeInUpOpacity = css<{ isShow: boolean }>`
+  opacity: 0;
+  animation: ${props =>
+    props.isShow
+      ? css`
+          ${fadeInUp} 0.5s ease forwards
+        `
+      : "none"};
+`;
+
+const BannerContainer = styled.div<{ isShow: boolean }>`
+  will-change: transform, opacity;
+  height: 100vh;
+  display: ${({ isShow }) => (isShow ? "flex" : "visibilty")};
+  flex-direction: column;
+  align-items: center;
+  ${fadeInUpOpacity}
+`;
+
+const MainTitle = styled.h2<{ isShow: boolean }>`
+  margin-top: 15rem;
+  font-size: 2.5rem;
+  ${fadeInUpOpacity}
+  animation-delay: 0.2s;
+`;
+
+const SubTitle = styled.h3<{ isShow: boolean }>`
+  font-size: 2rem;
+  ${fadeInUpOpacity}
+  animation-delay: 0.4s;
+`;
+const ImageContainer = styled.div`
+  border: 1px solid #ddd;
+  box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);
+  padding: 10px;
+  border-radius: 10px;
+  transition: transform 0.3s ease, box-shadow 0.3s ease;
+  margin-top: 3rem;
+
+  &:hover {
+    transform: scale(1.05);
+    box-shadow: 0 6px 12px rgba(0, 0, 0, 0.2);
+  }
+`;
