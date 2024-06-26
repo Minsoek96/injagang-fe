840680324ae899876a1bdfb9a6069004bc9d2e5f
Minsoek96 <FE.minseok96@gmail.com>
2024-06-26 21:21:56 +0900

    fix : baseURL 미수정

--- client/util/fecher.tsx
+++ client/util/fecher.tsx
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-axios.defaults.baseURL = "http://localhost:8080";
+axios.defaults.baseURL = process.env.BACKEND_SERVER_API;
 export enum METHOD {
   GET = "get",
   POST = "post",
