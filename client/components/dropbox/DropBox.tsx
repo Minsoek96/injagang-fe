d1b7f1ecab9ea0526a174e82e7be4286d48a5099
Minsoek96 <FE.minseok96@gmail.com>
2024-07-11 20:53:47 +0900

    feat : 드롭박스 컴포넌트

---
+++ client/components/dropbox/DropBox.tsx
@@ -0,0 +1,49 @@
+import { useState } from 'react';
+
+import { CSSProperties, styled } from 'styled-components';
+
+import DropMenuList from './DropMenuList';
+
+import { DropItemType } from './types';
+
+type DropBoxProps = {
+  buttonContent: React.ReactNode;
+  dropStyles?: CSSProperties;
+  dropList: DropItemType[];
+};
+
+export default function DropBox({
+  buttonContent,
+  dropStyles = {},
+  dropList = [],
+}: DropBoxProps) {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const handleDropToggle = () => {
+    setIsOpen((props) => !props);
+  };
+
+  return (
+    <Container style={dropStyles}>
+      <DropToggleButton onClick={handleDropToggle} title="drop-btn">
+        {buttonContent}
+      </DropToggleButton>
+      {isOpen && <DropMenuList dropList={dropList} />}
+    </Container>
+  );
+}
+
+const Container = styled.div`
+  position: relative;
+  display: inline-block;
+`;
+
+const DropToggleButton = styled.button`
+    border: none;
+    background-color: ${(props) => props.theme.colors.primary};
+    cursor: pointer;
+
+    p{
+      display: none;
+    }
+`;
