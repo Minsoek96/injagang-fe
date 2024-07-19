fff352a1d1c1379369a5fa5dcad356ce429bab5a
Minsoek96 <FE.minseok96@gmail.com>
2024-07-19 23:35:08 +0900

    feat : useCheckList 테스트 코드 작성

--- client/fixutures/shared/index.ts
+++ client/fixutures/shared/index.ts
@@ -14,3 +14,26 @@ export const sampleModal = {
     message: 'TEST_MODAL',
   },
 };
+
+export const sampleCheckList = [
+  {
+    id: 1,
+    title: '테스트1',
+  },
+  {
+    id: 2,
+    title: '테스트2',
+  },
+  {
+    id: 3,
+    title: '테스트3',
+  },
+  {
+    id: 4,
+    title: '테스트4',
+  },
+  {
+    id: 5,
+    title: '테스트5',
+  },
+];
