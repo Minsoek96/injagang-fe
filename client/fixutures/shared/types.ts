33fb16666214f3ce18d42d36024c6cb3d6f1a222
Minsoek96 <FE.minseok96@gmail.com>
2024-07-21 21:24:50 +0900

    feat : useWebSpeech 훅 테스트 코드 작성

--- client/fixutures/shared/types.ts
+++ client/fixutures/shared/types.ts
@@ -13,7 +13,37 @@ type MockStream = {
 };
 
 type MockHTMLVideoElement = {
-    srcObject: MediaStream | null;
+  srcObject: MediaStream | null;
 };
 
-export { type MockMediaRecorder, type MockStream, type MockHTMLVideoElement };
+type MockUtterance = {
+  lang: string | null;
+  onboundary: null;
+  onend: jest.Mock;
+  onerror: null;
+  onmark: null;
+  onpause: null;
+  onresume: null;
+  onstart: null;
+  pitch: number;
+  rate: number;
+  text: string;
+  voice: null;
+  volume: number;
+};
+
+type MockSpeech = {
+  onvoiceschanged: null,
+  paused: boolean,
+  pending: boolean,
+  speacking: boolean,
+  speak: jest.Mock
+}
+
+export {
+  type MockMediaRecorder,
+  type MockStream,
+  type MockHTMLVideoElement,
+  type MockUtterance,
+  type MockSpeech,
+};
