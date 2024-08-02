f58ccdf66a86c6776a26f432c7f5ccc60d89e852
Minsoek96 <FE.minseok96@gmail.com>
2024-08-02 16:40:00 +0900

    feat : 공용 Input 생성 테스트 코드 완료

---
+++ client/src/shared/components/input/MainInput.tsx
@@ -0,0 +1,37 @@
+import { CSSProperties, styled } from 'styled-components';
+
+type Props = {
+  placeholder?: string;
+  value: string;
+  onChange: (text: string) => void;
+  id?: string;
+  sx?: CSSProperties;
+};
+
+export default function MainInput({
+  placeholder = '',
+  value,
+  onChange,
+  id = '',
+  sx = {},
+}: Props) {
+  return (
+    <Input
+      type="text"
+      placeholder={placeholder}
+      value={value}
+      onChange={(e) => onChange(e.target.value)}
+      id={id}
+      style={sx}
+    />
+  );
+}
+
+const Input = styled.input`
+  height: 4rem;
+  border-radius: 0.8rem;
+  border: 0.1em solid ${(props) => props.theme.colors.mainLine};
+  background-color: ${(props) => props.theme.colors.secondary};
+  color: ${(props) => props.theme.colors.text};
+  padding: 0.8em 0.16em;
+`;
