3e6a612747e291a12ff2c6ef3b051f5a15bf5ec9
Minsoek96 <FE.minseok96@gmail.com>
2024-08-20 22:14:25 +0900

    refact : interviewRecord 메인 컴포넌트 수정
    - 복잡한 상태 관리를 삭제
    - interviewMode로 현재의 컴포넌트 관리를 단순화
    - 녹화에 사용될 스피치 데이터를 관리

--- client/src/entities/interview_question/useRecordInfoStore.ts
+++ client/src/entities/interview_question/useRecordInfoStore.ts
@@ -5,11 +5,11 @@ type RecordInfo = {
   timer: string;
 };
 
-type VideoSatus = 'record' | 'pause' | 'pending';
+type InterviewMode = 'pending' | 'record' | 'result';
 
 type State = {
-  videoStatus: VideoSatus;
-  curVideoMode: 'result' | 'interview';
+  interviewMode: InterviewMode;
+  recordedChunks: Blob[];
   audioDevice: MediaDeviceInfo | null;
   videoDevice: MediaDeviceInfo | null;
   recordInfoList: RecordInfo[];
@@ -20,51 +20,58 @@ type State = {
 type Action = {
   addRecordInfo: (info: RecordInfo) => void;
   initRecordInfoList: () => void;
+
   setCurTimer: (timer: string) => void;
   setCurScript: (script: string) => void;
   initCurinfos: () => void;
+
   setAudioDevice: (devices: MediaDeviceInfo) => void;
   setVideoDevice: (devices: MediaDeviceInfo) => void;
-  setVideoStatus: (state: VideoSatus) => void;
   initDevices: () => void;
-  toggleVideoMode: () => void;
+
+  setRecordedChunks: (chunk: Blob[]) => void;
+  setInterviewMode: (mode: InterviewMode) => void;
 };
 
 /**
- * videoStatus : "현재 녹화 진행상태"
- * curVideoMode : "유저 화면 선택상황"
- * devices : "녹화 장치 연결 장치 아이디"
- * recordInfoList : "유저가 녹화한 순서"
+ * interviewMode : "현재 인터뷰 모드를 의미"
+ * devices : "녹화 장치 연결 장치 고유 아이디"
+ * recordInfoList : "유저가 녹화한 순서별 스크립트 기록"
  * curScript: "현재 녹화 입력 대본 기록"
  * curTimer: "현재 녹화 타이머 기록"
  */
 
 const useRecordInfoStore = create<State & Action>((set) => ({
-  videoStatus: 'pending',
-  curVideoMode: 'interview',
+  interviewMode: 'record',
   audioDevice: null,
   videoDevice: null,
   curScript: '',
   curTimer: '',
   recordInfoList: [],
+  recordedChunks: [],
+
   setCurTimer: (curTimer: string) => set({ curTimer }),
   setCurScript: (curScript: string) => set({ curScript }),
+  initCurinfos: () => set({ curTimer: '', curScript: '' }),
+
   addRecordInfo: (newInfo: RecordInfo) => {
     set((state) => ({
       recordInfoList: [...state.recordInfoList, newInfo],
     }));
   },
-  initRecordInfoList: () => set({ recordInfoList: [] }),
-  initCurinfos: () => set({ curTimer: '', curScript: '' }),
-  setVideoStatus: (state: VideoSatus) => set({ videoStatus: state }),
-  toggleVideoMode: () => {
-    set((state) => ({
-      curVideoMode: state.curVideoMode === 'interview' ? 'result' : 'interview',
-    }));
-  },
+  initRecordInfoList: () =>
+    set({ recordInfoList: [], recordedChunks: [], interviewMode: 'record' }),
+
   setAudioDevice: (device: MediaDeviceInfo) => set({ audioDevice: device }),
   setVideoDevice: (device: MediaDeviceInfo) => set({ videoDevice: device }),
   initDevices: () => set({ audioDevice: null, videoDevice: null }),
+
+  setRecordedChunks: (recordedChunks: Blob[]) => {
+    set((state) => ({
+      recordedChunks: [...state.recordedChunks, ...recordedChunks],
+    }));
+  },
+  setInterviewMode: (mode: InterviewMode) => set({ interviewMode: mode }),
 }));
 
 export default useRecordInfoStore;
