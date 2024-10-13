5b90db8566e47afcfbeb5d3c38e644463eaf0ff4
Minsoek96 <FE.minseok96@gmail.com>
2024-10-13 23:59:35 +0900

    feat :  paswordForm - 패스워드 변경 폼

--- client/src/entities/auth/index.ts
+++ client/src/entities/auth/index.ts
@@ -2,5 +2,6 @@ export * as authMutations from './api/mutations';
 export * as authApi from './api/apis';
 export * as authType from './model/type';
 export * as authSchema from './model/schema';
+export * from './ui/common-style';
 export { default as useAuthStore } from './model/useAuthStore';
 export { default as useAuth } from './model/useAuth';
--- client/src/entities/auth/model/schema.ts
+++ client/src/entities/auth/model/schema.ts
@@ -1,11 +1,24 @@
 import { z } from 'zod';
 
-export const password = z.object({
-  nowPassword: z.string().min(1, '현재 비밀번호를 입력해주세요.'),
-  changePassword: z.string()
-    .min(8, '비밀번호는 최소 8자리여야 합니다.')
-    .regex(/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[A-Za-z\d@$!%*?&]{8,}$/, '비밀번호는 대문자, 소문자, 숫자를 포함해야 합니다.'),
-}).refine((data) => data.changePassword === data.nowPassword, {
-  message: '비밀번호가 일치하지 않습니다.',
-  path: ['changePasswordCheck'],
-});
+export const password = z
+  .object({
+    nowPassword: z.string().min(1, '현재 비밀번호를 입력해주세요.'),
+    changePassword: z
+      .string()
+      .min(8, '비밀번호는 최소 8자리여야 합니다.')
+      .regex(
+        /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[A-Za-z\d@$!%*?&]{8,}$/,
+        '비밀번호는 대문자, 소문자, 숫자를 포함해야 합니다.',
+      ),
+    changePasswordCheck: z
+      .string()
+      .min(8, '비밀번호는 최소 8자리여야 합니다.')
+      .regex(
+        /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[A-Za-z\d@$!%*?&]{8,}$/,
+        '비밀번호는 대문자, 소문자, 숫자를 포함해야 합니다.',
+      ),
+  })
+  .refine((data) => data.changePassword === data.changePasswordCheck, {
+    message: '비밀번호가 일치하지 않습니다.',
+    path: ['changePasswordCheck'],
+  });
