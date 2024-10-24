f75de2534e3a6fd13695112fd605c2968f240eb3
Minsoek96 <FE.minseok96@gmail.com>
2024-10-24 23:19:34 +0900

    feat : features/qna/feedback 관련 테스트 코드 작성 완료

--- client/src/features/qna/feedback/TargetFeedBackView.test.tsx
+++ client/src/features/qna/feedback/TargetFeedBackView.test.tsx
@@ -1,3 +1,4 @@
+import { sampleResponseFeed } from '@/fixutures/entities/feed';
 import TestProvider from '@/fixutures/TestProvider';
 import { feedbackQueries } from '@/src/entities/feedback';
 import { IGetFeedBack } from '@/src/entities/feedback/model/type';
@@ -22,21 +23,49 @@ describe('TargetFeedBackView', () => {
       data,
     });
   };
-  beforeEach(() => {});
-  const renderComponent = () => {
+
+  beforeEach(() => {
+    jest.clearAllMocks();
+  });
+
+  const renderComponent = (isTarget: boolean = false) => {
     render(
       <TestProvider>
-        <TargetFeedBackView targetFeed={0} />
+        <TargetFeedBackView targetFeed={isTarget ? 10001 : 0} />
       </TestProvider>,
     );
   };
 
-  context('feedbackList가 존재하지 않는 경우', () => {
-    it('안내 메시지를 전달한다.', () => {
-      mockFeedbackList([]);
+  context('타겟 넘버가 존재하는 상황', () => {
+    context('feedbackList(피드백 목록)이 존재 하지 않으면.', () => {
+      it('목록이 없는 상황을 전달한다.', () => {
+        mockFeedbackList([]);
+        renderComponent(true);
+        const searchEmpty = screen.getByText(/등록된 피드백이 없습니다/);
+        expect(searchEmpty).toBeInTheDocument();
+      });
+    });
+
+    context('타겟이 존재하는 상황에서는', () => {
+      it('비어있는 상태를 전달한다.', () => {
+        mockFeedbackList([]);
+        renderComponent();
+        const searchEmpty = screen.getByText(/자소서 넘버를 선택해주세요/);
+        expect(searchEmpty).toBeInTheDocument();
+      });
+    });
+  });
+
+  context('feedbackList가 존재하는 경우', () => {
+    it('리스트의 아이템이 렌더링된다.', () => {
+      mockFeedbackList(sampleResponseFeed);
       renderComponent();
-      const searchEmpty = screen.getByText(/자소서 넘버를 선택해주세요/);
-      expect(searchEmpty).toBeInTheDocument();
+      sampleResponseFeed.forEach((feed) => {
+        const searchCorrection = screen.getByText(feed.target);
+        const searchFeed = screen.getByText(feed.content);
+        expect(searchCorrection).toBeInTheDocument();
+        expect(searchFeed).toBeInTheDocument();
+      });
     });
   });
 });
