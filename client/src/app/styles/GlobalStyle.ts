34b964aeebb9ad7397c4072f2a67dae7c9367c35
Minsoek96 <FE.minseok96@gmail.com>
2024-12-01 21:55:23 +0900

    feat : pretendardFont

--- client/src/app/styles/GlobalStyle.ts
+++ client/src/app/styles/GlobalStyle.ts
@@ -1,6 +1,14 @@
 import { createGlobalStyle } from 'styled-components';
 
 const GlobalStyle = createGlobalStyle`
+  @font-face {
+    font-family: 'Pretendard';
+    src: url('/fonts/PretendardVariable.woff2') format('woff2-variations');
+    font-weight: 45 920;
+    font-style: normal;
+    font-display: swap;
+  }
+
   html {
     box-sizing: border-box;
     font-size: 62.5% !important;
@@ -16,13 +24,19 @@ const GlobalStyle = createGlobalStyle`
     font-size: 1.6rem;
     background: ${(props) => props.theme.colors.bodyColor};
     color: ${(props) => props.theme.colors.text};
-    font-family: 'Roboto', sans-serif;
+    font-family: 'Pretendard', -apple-system, BlinkMacSystemFont, system-ui,
+    "Helvetica Neue", "Segoe UI", "Apple SD Gothic Neo", "Noto Sans KR", "Malgun Gothic", sans-serif;
   }
 
   :lang(ko) {
+    font-family: 'Pretendard', -apple-system, "Apple SD Gothic Neo", "Noto Sans KR", "Malgun Gothic", sans-serif;
     h1, h2, h3 {
       word-break: keep-all;
     }
+  }
+
+  :lang(en) {
+    font-family: 'Roboto', -apple-system, BlinkMacSystemFont, system-ui, "Helvetica Neue", "Segoe UI", sans-serif;
   }
 
   .sr-only {
