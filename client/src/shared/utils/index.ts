96b90d20f44aa6d8c26b7905e310bab402733e67
Minsoek96 <FE.minseok96@gmail.com>
2025-03-21 14:23:01 +0900

    refact : 유효성 검사 유틸 로직 개선

--- client/src/shared/utils/index.ts
+++ client/src/shared/utils/index.ts
@@ -3,7 +3,7 @@ export * as hasEmpty from './check/hasEmpty';
 export * as formatTime from './format/formatTime';
 export { default as getServerCookie } from './cookies/getServerCookies';
 export { default as keys } from './format/keys';
-export { default as runValidationChecks } from './check/runValidationChecks';
+export { default as validateRules } from './check/validateRules';
 export { default as SessionStorageManager } from './storage/sessionStorageManager';
 export { default as LocalStorageManager } from './storage/localStorageManager';
 export * from './cookies/cookiesManager';
