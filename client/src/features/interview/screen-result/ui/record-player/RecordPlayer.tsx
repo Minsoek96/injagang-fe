15eb3de7638adb1e66433d2ce077e672c5236b51
Minsoek96 <FE.minseok96@gmail.com>
2025-04-03 03:13:28 +0900

    fix : interview/screen-result record-player 한번씩 URL 생성 관련 문제

--- client/src/features/interview/screen-result/ui/record-player/RecordPlayer.tsx
+++ client/src/features/interview/screen-result/ui/record-player/RecordPlayer.tsx
@@ -1,30 +1,42 @@
-import { V } from '@/src/shared/styles';
-import { useEffect, useRef } from 'react';
+import { useEffect, useState } from 'react';
+
 import { styled } from 'styled-components';
 
+import { V } from '@/src/shared/styles';
+import { Spinner } from '@/src/shared/ui';
+
 type Props = {
-    currentVideoChunk: Blob
-}
+  currentVideoChunk: Blob;
+};
 
-export default function RecordPlayer({ currentVideoChunk }:Props) {
-  const videoUrl = useRef<string>(URL.createObjectURL(currentVideoChunk));
+export default function RecordPlayer({ currentVideoChunk }: Props) {
+  const [videoUrl, setVideoUrl] = useState<string>('');
 
-  useEffect(() => () => {
-    URL.revokeObjectURL(videoUrl.current); // 메모리 누수 방지를 위해 URL 해제
-  }, []);
+  useEffect(() => {
+    if (!currentVideoChunk) return;
 
-  if (!currentVideoChunk) {
-    return <p>비디오를 로드 중입니다...</p>;
+    if (videoUrl) {
+      URL.revokeObjectURL(videoUrl);
+    }
+
+    const newUrl = URL.createObjectURL(currentVideoChunk);
+    setVideoUrl(newUrl);
+
+    // eslint-disable-next-line consistent-return
+    return () => {
+      URL.revokeObjectURL(newUrl);
+    };
+  }, [currentVideoChunk]);
+
+  if (!videoUrl) {
+    return (
+      <VideoLoaderContainer>
+        <Spinner message="비디오를 로드 중입니다" />
+      </VideoLoaderContainer>
+    );
   }
 
-  return (
-    <PlayerWrapper
-      autoPlay
-      controls
-      playsInline
-      src={URL.createObjectURL(currentVideoChunk)}
-    />
-  );
+  return <PlayerWrapper autoPlay controls playsInline src={videoUrl} />;
 }
 
 const PlayerWrapper = styled.video`
@@ -38,6 +50,18 @@ const PlayerWrapper = styled.video`
   border-radius: 0.8rem;
 
   @media screen and (max-width: ${V.mediaMobile}) {
-   width: 100%;
- }
+    width: 100%;
+  }
+`;
+
+const VideoLoaderContainer = styled.div`
+  height: 100%;
+  width: 100%;
+  max-height: 50rem;
+  min-height: 30rem;
+  background-color: ${(props) => props.theme.colors.primary};
+  border-top: 1px dashed ${(props) => props.theme.colors.highlightColor};
+  border-bottom: 1px dashed ${(props) => props.theme.colors.highlightColor};
+  border-radius: 0.8rem;
+  padding: 2rem;
 `;
